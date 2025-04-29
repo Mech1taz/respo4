bf8b69b7074f415f07aedc3e1ff43e7bde842b76
MechitaZ <ma.astudillom@duocuc.cl>
2025-04-29 14:22:43 -0400

    enter

--- src/main/java/com/example/respo4/controller/PersonaController.java
+++ src/main/java/com/example/respo4/controller/PersonaController.java
@@ -1,17 +1,32 @@
 package com.example.respo4.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.respo4.model.Persona;
+import com.example.respo4.service.PersonaService;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 
 @RequestMapping("/api/personas")
 @RestController //devuelve datos :D por eso el Rest
 
 public class PersonaController {
+    @Autowired
+    private PersonaService personaService;
 
     @GetMapping
     public String saludar()
     {
         return "Hola esto esta funcionando... o debería uwu";
     }
+    @PostMapping 
+    public Persona guardar(@RequestBody Persona persona){
+        return personaService.guardarPersona(persona);
+    }
+    
 }
